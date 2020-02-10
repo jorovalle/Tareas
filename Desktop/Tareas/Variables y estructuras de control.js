5c05b774aaacce860e90b41e1619e9dc8e98a4c1
Carlos Ovalle <jorovalle@gmail.com>
2020-02-09 20:26:01 -0600

    first commit

---
+++ Desktop/Tareas/Variables y estructuras de control.js
@@ -0,0 +1,11 @@
+var mes = 10;
+if ((mes==12)||(mes ==1)||(mes==2)){
+  alert("es invierno");
+}else if((mes>=3) && (mes<=5)){
+  alert("es primavera");
+}else if((mes>=6) && (mes<=8)){
+  alert("es verano");
+}else if((mes>=9) && (mes<=11)){
+  alert("es otonno");
+}
+alert("20042181 / Carlos Alexander Ovalle Lopez");
